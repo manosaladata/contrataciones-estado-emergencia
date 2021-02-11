c128bd5dd712ad17c5e9a2d8e1230c5687260dbd
abnercasallo <68797376+abnercasallo@users.noreply.github.com>
2021-02-11 00:37:55 -0500

    primer commit

---
+++ Roxygen/opencontracts/R/opencontracs.R
@@ -0,0 +1,9 @@
+#' OpenContracts package
+#' Contiene funciones para analizar los contratos directos por Covid en Perú
+#'
+#' @docType package
+#'
+#' @author Abner Francisco Casallo Trauco \email{abner.casallo@unmsm.edu.pe}
+#'
+#' @name opencontracts
+NULL
